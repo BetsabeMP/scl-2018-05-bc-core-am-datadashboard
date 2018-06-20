b74a02aea4b3cb116c3b9fd13e1cc91bb2161d67
Betsabé Mique <betsabe.mique.p@gmail.com>
2018-06-20 18:17:59 -0400

    cambios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-
+// función al cargar página carga funciones de datos y escritura
 window.onload = () => {
-  country();
-};
-
-// función para obtener paises y escribirlos
-const country = () => {
   window.callCountry()
     .then((dataCountry) => {
       const dataCountryTrue = dataCountry.filter(country => country.active !== false);
@@ -21,6 +16,30 @@ const cohort = (campus) => {
       .then((dataCohorts) => {
         window.writeCohort(dataCohorts);
         dataCohorts.map(cohort => course(cohort.id));
+        dataCohorts.map(cohort => user(cohort.id + 'a'));
+      });
+  });
+};
+
+// función para obtener usuarios según el cohort y escribirlos
+const user = (cohorta) => {
+  const btn = document.getElementById(cohorta);
+  btn.addEventListener('click', () => {
+    window.callUsers(cohorta)
+      .then((dataUsers) => {
+        window.writeUser(dataUsers);
+        dataUsers.map(users => userInput(users.id));
+        dataUsers.map(users => UserCohort(users.id, users.signupCohort));
+      });
+  });
+};
+
+// función para buscar nombre desde input
+const userInput = (users) => {
+  const inpt = document.getElementById('inputButton');
+  inpt.addEventListener('click', () => {
+    window.callUserInput(users)
+      .then((dataUserInput) => {
       });
   });
 };
@@ -31,8 +50,24 @@ const course = (cohort) => {
   btn.addEventListener('click', () => {
     window.callCourses(cohort)
       .then((dataCourses) => {
-        console.log(dataCourses);
         window.writeCourse(dataCourses);
+      });
+  });
+};
+
+// función para obtener progreso de una alumna de un cohort 
+const UserCohort = (user, cohortId) => {
+  const btn = document.getElementById(user);
+  btn.addEventListener('click', () => {
+    window.callUsersCohort(cohortId)
+      .then((dataUserCohort) => {
+        const userCohortArr = Object.entries(dataUserCohort);
+        for (let i = 0; i < userCohortArr.length; i++) {
+          if (userCohortArr[i][0] === user) {
+            let found = userCohortArr[i];
+            console.log(found);
+          };
+        };
       });
   });
 };
