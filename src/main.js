e091385b4a657075f6d723710d1500b636b89ae8
Betsabé Mique <betsabe.mique.p@gmail.com>
2018-06-21 20:30:54 -0400

    cambios

--- src/main.js
+++ src/main.js
@@ -16,22 +16,23 @@ window.onload = () => {
 
 // función para obtener datos de cada tema y unidad
 const getUnits = (units) => {
-  console.log('units', units);
-  // declaro variables para las sumatorias
-  let summationCompleted = 0;
-  let summationDuration = 0;
-  let summationCompletedRead = 0;
-  let summationDurationRead = 0;
-  let summationCompletedPractice = 0;
-  let summationDurationPractice = 0;
-  let summationCompletedQuiz = 0;
-  let summationDurationQuiz = 0;
+  // console.log('units', units);
+
 
   // unitsKey obtengo subindice de unidades (0,1,2,...)
   const unitsKey = Object.keys(units);
 
   // recorro arreglo units segun sub indice y obtengo parts y percent
-  const unitDetail = unitsKey.map(unit => {
+
+  let totUnitSummationCompleted = 0;
+  let totUnitSummationDuration = 0;
+  let totUnitSummationCompletedRead = 0;
+  let totUnitSummationDurationRead = 0;
+  let totUnitSummationCompletedPractice = 0;
+  let totUnitSummationDurationPractice = 0;
+  let totUnitSummationCompletedQuiz = 0;
+  let totUnitSummationDurationQuiz = 0;
+  for (let unit of unitsKey) {
     const partObj = units[unit].parts;
     const percentObj = units[unit].percent;
 
@@ -39,16 +40,37 @@ const getUnits = (units) => {
     const partKey = Object.keys(units[unit].parts);
 
     // recorro arreglo partObj segun sub indice partKey y obtengo duraciones y type
-    const partDetail = partKey.map(part => {
+    // const partDetail = partKey.map(part => {
+
+    let totSummationCompleted = 0;
+    let totSummationDuration = 0;
+    let totSummationCompletedRead = 0;
+    let totSummationDurationRead = 0;
+    let totSummationCompletedPractice = 0;
+    let totSummationDurationPractice = 0;
+    let totSummationCompletedQuiz = 0;
+    let totSummationDurationQuiz = 0;
+
+    for (let part of partKey) {
       const completedUnit = partObj[part].completed;
       const durationUnit = partObj[part].duration;
       const typeUnit = partObj[part].type;
-      console.log('percentObj', percentObj, 'completedUnit', completedUnit, 'durationUnit', durationUnit, 'typeUnit', typeUnit);
+      // console.log('percentObj', percentObj, 'completedUnit', completedUnit, 'durationUnit', durationUnit, 'typeUnit', typeUnit);
+
+      // declaro variables para las sumatorias
+      let summationCompleted = 0;
+      let summationDuration = 0;
+      let summationCompletedRead = 0;
+      let summationDurationRead = 0;
+      let summationCompletedPractice = 0;
+      let summationDurationPractice = 0;
+      let summationCompletedQuiz = 0;
+      let summationDurationQuiz = 0;
 
       // sumo completed y duration 
       summationCompleted = summationCompleted + completedUnit;
       summationDuration = summationDuration + durationUnit;
-      console.log('summationCompleted', summationCompleted, 'summationDuration', summationDuration);
+      // console.log('summationCompleted', summationCompleted, 'summationDuration', summationDuration);
 
       // sumo con read
       if (typeUnit === 'read') {
@@ -60,7 +82,7 @@ const getUnits = (units) => {
       };
       summationCompletedRead = summationCompletedRead + completedUnitRead;
       summationDurationRead = summationDurationRead + durationUnitRead;
-      console.log('summationCompletedRead', summationCompletedRead, 'summationDurationRead', summationDurationRead);
+      // console.log('summationCompletedRead', summationCompletedRead, 'summationDurationRead', summationDurationRead);
 
       // sumo con practice
       if (typeUnit === 'practice') {
@@ -72,7 +94,7 @@ const getUnits = (units) => {
       };
       summationCompletedPractice = summationCompletedPractice + completedUnitPractice;
       summationDurationPractice = summationDurationPractice + durationUnitPractice;
-      console.log('summationCompletedPractice', summationCompletedPractice, 'summationDurationPractice', summationDurationPractice);
+      // console.log('summationCompletedPractice', summationCompletedPractice, 'summationDurationPractice', summationDurationPractice);
 
       // sumo con quiz
       if (typeUnit === 'quiz') {
@@ -84,11 +106,37 @@ const getUnits = (units) => {
       };
       summationCompletedQuiz = summationCompletedQuiz + completedUnitQuiz;
       summationDurationQuiz = summationDurationQuiz + durationUnitQuiz;
-      console.log('summationCompletedQuiz', summationCompletedQuiz, 'summationDurationQuiz', summationDurationQuiz);
-    });
-    return partDetail;
-  });
-  return unitDetail;
+      // console.log('summationCompletedQuiz', summationCompletedQuiz, 'summationDurationQuiz', summationDurationQuiz);
+
+      totSummationCompleted += summationCompleted;
+      totSummationDuration += summationDuration;
+      totSummationCompletedRead += summationCompletedRead;
+      totSummationDurationRead += summationDurationRead;
+      totSummationCompletedPractice += summationCompletedPractice;
+      totSummationDurationPractice += summationDurationPractice;
+      totSummationCompletedQuiz += summationCompletedQuiz;
+      totSummationDurationQuiz += summationDurationQuiz;
+    };
+
+    totUnitSummationCompleted += totSummationCompleted;
+    totUnitSummationDuration += totSummationDuration;
+    totUnitSummationCompletedRead += totSummationCompletedRead;
+    totUnitSummationDurationRead += totSummationDurationRead;
+    totUnitSummationCompletedPractice += totSummationCompletedPractice;
+    totUnitSummationDurationPractice += totSummationDurationPractice;
+    totUnitSummationCompletedQuiz += totSummationCompletedQuiz;
+    totUnitSummationDurationQuiz += totSummationDurationQuiz;
+  };
+  return {
+    totUnitSummationCompleted,
+    totUnitSummationDuration,
+    totUnitSummationCompletedRead,
+    totUnitSummationDurationRead,
+    totUnitSummationCompletedPractice,
+    totUnitSummationDurationPractice,
+    totUnitSummationCompletedQuiz,
+    totUnitSummationDurationQuiz
+  };
 };
 
 // función para obtener unidades y courses  desde la info de alumna
@@ -104,11 +152,8 @@ const getCourseInfo = (user) => {
 
 // función calcula porcentaje total
 const getPercentCourse = (user) => {
-  console.log('user', user);
-
   // user es un objeto x eso uso Object.keys para obtener un arreglo solo con las keys
   const courses = Object.keys(user);
-  console.log('course', courses);
   // recorra el arreglo user.course(basic..),percent y que lo redusca a un valor que es la sumatoria 
   const percentList = courses.map(course => user[course].percent).reduce((tot, value) => tot + value);
   // la sumatoria/cantidad de unidades y obtengo porcentaje total
@@ -116,17 +161,17 @@ const getPercentCourse = (user) => {
 };
 
 // funcion para trabajar datos de alumnas
-const infoUser = (user) => {
+const getInfoUser = (user) => {
   // guarda la info que obtienes al ejecutar función getPercentCourse en la variable percentCourse
   const percentCourse = getPercentCourse(user);
 
   // idem
   const courseInfo = getCourseInfo(user);
 
-  console.log('salida', {
+  return {
     percentCourse,
     courseInfo
-  });
+  };
 };
 
 // función para obtener progreso de una alumna de un cohort 
@@ -139,7 +184,8 @@ const userCohort = (user, cohortId) => {
       .then((datauserCohort) => {
         // obtengo un objeto
         // ejecuta la funcion infoUser dandole datauserCohort.user necesarios para trabajar con el progreso de la alumna
-        infoUser(datauserCohort[user]);
+        const infoUser = getInfoUser(datauserCohort[user]);
+        console.info('salida', infoUser);
       });
   });
 };
